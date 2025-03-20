bbc3f1a3d8dc8754e1efff2c62585a4241b65566
guilhermesaud <guilhermesaud31@gmail.com>
2025-03-19 23:25:51 -0300

    Add msgbox dialog for remote services

--- shells/toggle_remote_services.sh
+++ shells/toggle_remote_services.sh
@@ -14,7 +14,6 @@ CheckRemoteServices() {
 CheckRemoteServices
 if [ $? -eq 0 ]; then
     {
-        echo "Desativando servicos remotos..."
         sudo systemctl disable NetworkManager-wait-online
         sudo systemctl stop NetworkManager-wait-online
         sudo timedatectl set-ntp 0
@@ -22,7 +21,7 @@ if [ $? -eq 0 ]; then
         sudo systemctl stop nmbd
         sudo systemctl stop ssh.service
         sudo pkill filebrowser
-        echo "Servicos remotos desativados."
+        dialog --msgbox "Servicos remotos desativados." 6 40
     }
 else 
     {
@@ -30,19 +29,15 @@ else
         sudo systemctl start NetworkManager-wait-online
         GW=`ip route | awk '/default/ { print $3 }'`
         if [ ! -z "$GW" ]; then
-          echo "Ativando servicos remotos..."
           sudo timedatectl set-ntp 1 &
           sudo systemctl start smbd
           sudo systemctl start nmbd
           sudo systemctl start ssh.service
-          sudo filebrowser -a 0.0.0.0 -p 80 -d /home/ark/.config/filebrowser.db -r / &
-          echo "SUCESSO!"
-          echo "IP: $(hostname -I | awk '{print $1}')"
-          sleep 2
+          sudo filebrowser -a 0.0.0.0 -p 80 -d /home/ark/.config/filebrowser.db -r / > /dev/null 2>&1 &
+          IP=$(hostname -I | awk '{print $1}')
+          dialog --msgbox "Servicos remotos ativados!\nIP: $IP" 6 40
         else
-          echo "ERRO: Conexao de rede nao detectada!"
-          echo "Verifique sua rede Wi-Fi e tente novamente."
-          sleep 2
+          dialog --msgbox "ERRO: Conexao de rede nao detectada!\nVerifique sua rede Wi-Fi e tente novamente."
         fi
     } 
 fi
